e9f9bf4a482cfc15eb39f844ba0bcda80b7d535e
Aziz Al-Darrab <aziz_darrab@hotmail.com>
2020-11-24 23:45:14 +0300

    Add project files.

---
+++ SwipeBindingBug/SwipeBindingBug/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using SwipeBindingBug.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SwipeBindingBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new PageViewModel();
+        }
+    }
+}
---
+++ SwipeBindingBug/SwipeBindingBug/ViewModels/PageViewModel.cs
@@ -0,0 +1,58 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SwipeBindingBug.ViewModels
+{
+
+    public class PageViewModel : BaseViewModel
+    {
+        public ObservableRangeCollection<string> Items { get; set; } = new ObservableRangeCollection<string>();
+        private string buttonLabel;
+        private bool isSwipeViewEnabled;
+        public ICommand SwitchSwipeViewStatus => new Command(SwitchSwipe);
+        public string ButtonLabel
+        {
+            get
+            {
+                return buttonLabel;
+            }
+            set
+            {
+                buttonLabel = value;
+                this.OnPropertyChanged("ButtonLabel");
+            }
+        }
+
+        public bool IsSwipeViewEnabled
+        {
+            get
+            {
+                return isSwipeViewEnabled;
+            }
+            set
+            {
+                isSwipeViewEnabled = value;
+                this.OnPropertyChanged("IsSwipeViewEnabled");
+            }
+        }
+
+        public PageViewModel()
+        {
+            Items.AddRange(new List<string> { "Baboon", "Capuchin Monkey", "Blue Monkey", "Squirrel Monkey", "Golden Lion Tamarin", "Howler Monkey", "Japanese Macaque" });
+            IsSwipeViewEnabled = true;
+            ButtonLabel = $"Is SwipeView Enabled: {IsSwipeViewEnabled}";
+        }
+        public void SwitchSwipe()
+        {
+            IsSwipeViewEnabled = !IsSwipeViewEnabled;
+            ButtonLabel = $"Is SwipeView Enabled: {IsSwipeViewEnabled}";
+        }
+    }
+
+    
+}
